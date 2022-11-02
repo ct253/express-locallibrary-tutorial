090e3d366dddf49e3125e1a0b25edc83252c809b
ct253 <35614892+ct253@users.noreply.github.com>
2022-11-01 21:50:40 -0700

    Added files and changes required for deployment

--- app.js
+++ app.js
@@ -44,8 +44,9 @@ module.exports = app;
 
 // Set up mongoose connection
 const mongoose = require("mongoose");
-const mongoDB =
+const dev_db_url =
   "mongodb+srv://colincushman:WZyQZPUNb76r0Vqs@cluster0.cmhit0e.mongodb.net/?retryWrites=true&w=majority";
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
